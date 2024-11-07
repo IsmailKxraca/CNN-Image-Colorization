8ac958c3d6fda661a28d99d980df6e8251905c0d
Ismail Karaca <Ismailkaracayt@gmail.com>
2024-11-07 09:08:46 +0100

    data preprocessing steps

---
+++ scripts/data_prep.py
@@ -0,0 +1,28 @@
+"""
+In this script, i will prepare the dataset for the usage in the CNN
+This includes:
+ - turning them into 256x256
+ - turn them into LAB
+ - extract A and B, turn into classes (These will be the labels)
+ - use L as grayscale image for training and testing
+"""
+
+
+def scale_image():
+    # 256x256
+    pass
+
+
+def turn_image_lab():
+    pass
+
+
+def extract_labels():
+    # take ab values and classify them into one of the bins.
+    # save the labels
+    pass
+
+
+def turn_into_grayscale():
+    pass
+
---
+++ scripts/lab_quantization.py
@@ -0,0 +1,18 @@
+"""
+In this script i will quantize the ab colorspace into discrete classes.
+I will use binning and clustering to identify which colors get used in reality and which are not.
+
+I will code functions for extracting the ab values of a Lab-image and classify them into the color-classes.
+"""
+
+
+def lab_bins():
+    pass
+
+
+def quantize():
+    pass
+
+
+def map_colorspace():
+    pass
