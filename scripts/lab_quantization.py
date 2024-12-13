56d8df9da051bd51ff7a7d286dce3acf6168c091
Ismail Karaca <ismailkaracayt@gmail.com>
2024-12-13 01:13:52 +0100

    important functions for binning the colorspace

--- scripts/lab_quantization.py
+++ scripts/lab_quantization.py
@@ -4,25 +4,42 @@ I will use binning and clustering to identify which colors get used in reality a
 
 I will code functions for extracting the ab values of a Lab-image and classify them into the color-classes.
 """
+import numpy as np
+
+# parameters for bins
+bin_size = 10
+min_val = -128
+max_val = 127
 
 
-def lab_bins():
-    pass
+def split_lab_channels(labimg):
+    l_channel = labimg[:, :, 0]
+    a_channel = labimg[:, :, 1]
+    b_channel = labimg[:, :, 2]
+
+    return l_channel, a_channel, b_channel
 
 
-def quantize():
-    pass
+def lab_bins(min_val, max_val, bin_size):
+    # creating the centers of the bins;
+    grid_points = np.arange(min_val + bin_size / 2, max_val, bin_size)
+    ab_grid = np.array(np.meshgrid(grid_points, grid_points)).T.reshape(-1, 2)
+    # two matrices with 313 values. One matrix for x and other for y values. Makes the grid
+    print("Grid Shape:", ab_grid.shape)
+    print(ab_grid)
+    return ab_grid
 
 
-def map_colorspace():
-    pass
+def find_nearest_bin(a_channel, b_channel, ab_grid):
+    # creates a vector with every pixel as ((a1,b1),(a2,b2) ...)
+    ab_values = np.stack((a_channel, b_channel), axis=-1).reshape(-1, 2)
+
+    # calculates the Euclidean distance between every pixel and every bin-center and saves it in a new dimension
+    distances = np.linalg.norm(ab_values[:, None, :] - ab_grid[None, :, :], axis=-1)
+
+    # finds the nearest bin-center for every pixel. vector with bin-center of every class (256x256)
+    nearest_bins = np.argmin(distances, axis=1)
+    return nearest_bins.reshape(a_channel.shape)
 
 
-def extract_labels():
-    # take ab values and classify them into one of the bins.
-    # save the labels
-    pass
-
-
-def extract_l(labimg):
-    pass
+lab_bins(min_val, max_val, bin_size)
