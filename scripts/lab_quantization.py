2671a79bed55936f24c12f8739ce1e9d11d1fede
Ismail Karaca <Ismailkaracayt@gmail.com>
2025-01-28 01:01:23 +0100

    bins visualisiert

--- scripts/lab_quantization.py
+++ scripts/lab_quantization.py
@@ -5,11 +5,14 @@ I will use binning and clustering to identify which colors get used in reality a
 I will code functions for extracting the ab values of a Lab-image and classify them into the color-classes.
 """
 import numpy as np
+from skimage.color import lab2rgb
+import matplotlib.pyplot as plt
+
 
 # parameters for bins
 bin_size = 10
-min_val = -128
-max_val = 127
+min_val = -110
+max_val = 110
 
 
 def split_lab_channels(labimg):
@@ -20,7 +23,7 @@ def split_lab_channels(labimg):
     return l_channel, a_channel, b_channel
 
 
-def lab_bins(min_val = -128, max_val = 127, bin_size = 10):
+def lab_bins(min_val = -110, max_val = 110, bin_size = 10):
     # creating the centers of the bins;
     grid_points = np.arange(min_val + bin_size / 2, max_val, bin_size)
     ab_grid = np.array(np.meshgrid(grid_points, grid_points)).T.reshape(-1, 2)
@@ -40,5 +43,37 @@ def find_nearest_bin(a_channel, b_channel, ab_grid):
     return nearest_bins.reshape(a_channel.shape)
 
 
-ab_grid = lab_bins(min_val, max_val, bin_size)
+def visualize_grid_colours(ab_grid, L=50):
+    """
+    visualizes the Colours of the bins-(centers)
+    With L=50
+    """
+    l_values = np.full((ab_grid.shape[0], 1), L)
+    lab = np.hstack((l_values, ab_grid))
 
+    # convert lab to rgb for illustration of colors
+    rgb = lab2rgb(lab.reshape(-1, 1, 3)).reshape(-1, 3)
+
+    # extract a and b values
+    a_values = ab_grid[:, 0]
+    b_values = ab_grid[:, 1]
+
+    # create diagram
+    plt.figure(figsize=(10, 8))
+    ax = plt.gca()
+
+    marker_size = (10 / (ax.get_xlim()[1] - ax.get_xlim()[0]) * 2)**2
+
+    # plot
+    plt.scatter(a_values, b_values, c=rgb, s=marker_size, marker='s', edgecolor='black')
+    ax.set_aspect('equal')
+    plt.title("Farben der Bin-Zentren im AB-Raum")
+    plt.xlabel("A-Wert")
+    plt.ylabel("B-Wert")
+    plt.grid(False)
+
+    # plot diagram
+    plt.show()
+
+ab_grid = lab_bins(min_val, max_val, bin_size)
+visualize_grid_colours(ab_grid)
