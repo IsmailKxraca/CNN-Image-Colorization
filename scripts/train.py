a1e6b446a3aaba8e7109638554c06e88b90fb9bf
Ismail Karaca <ismailkaracayt@gmail.com>
2025-02-13 12:36:22 +0100

    train changes

--- scripts/train.py
+++ scripts/train.py
@@ -11,6 +11,7 @@ from dataset import *
 
 batch_size = 8
 num_workers = 0
+epochs = 10
 
 project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 os.chdir(project_dir)
@@ -37,7 +38,6 @@ writer = SummaryWriter("runs/colorization_experiment")
 optimizer = optim.Adam(model.parameters(), lr=1e-5)
 criterion = nn.CrossEntropyLoss()
 
-epochs = 10
 scaler = torch.amp.GradScaler("cuda")
 
 for epoch in range(epochs):
@@ -81,8 +81,38 @@ for epoch in range(epochs):
 
     print(f"Checkpoint saved: {checkpoint_path}")
 
-writer.close()
 
 # save end model
 torch.save(model.state_dict(), "colorization_model.pth")
 print("Training finished!")
+
+
+# **Nach dem Training: Testen**
+print("Starte Testphase...")
+
+folder_L_test = os.path.join(os.path.abspath(os.path.join(os.getcwd(), os.pardir)), f"data{os.sep}preprocessed_data{os.sep}L_channel{os.sep}test")
+folder_ab_test = os.path.join(os.path.abspath(os.path.join(os.getcwd(), os.pardir)), f"data{os.sep}preprocessed_data{os.sep}AB_channel{os.sep}train")
+
+test_dataset = PreprocessedColorizationDataset(folder_L_test, folder_ab_test)
+test_loader=DataLoader(test_dataset, batch_size=8, shuffle=False, num_workers=4)
+model.eval()
+
+test_loss = 0.0
+
+with torch.no_grad():
+    loop = tqdm(test_loader, desc="Testphase")
+    for l_channel, ab_classes in loop:
+        l_channel, ab_classes = l_channel.to(device), ab_classes.to(device)
+
+        with torch.cuda.amp.autocast():
+            preds = model(l_channel)
+            loss = criterion(preds, ab_classes)
+
+        test_loss += loss.item()
+
+    avg_test_loss = test_loss / len(test_loader)
+    writer.add_scalar("Loss/test", avg_test_loss)
+    
+print(f"Durchschnittlicher Test-Loss: {avg_test_loss:.4f}")
+
+writer.close()
